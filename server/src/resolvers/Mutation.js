b167e4cc59119cc64cf53ebcb46a7d8b146aaa9d
Peter Daily <peter.daily@yahoo.co.uk>
2018-03-31 21:09:44 +0100

    add vote mutation

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,13 +1,23 @@
 function postPoll(parent, args, context, info) {
   const { name, options } = args
-  return context.db.mutation.createPoll(
-    { data: { name,
-      options: {
-        create: options
-      }
-     } }, info)
+  return context.db.mutation.createPoll({ 
+    data: { 
+      name,
+      options: { create: options }
+     } 
+    }, info)
+  }
+
+  function vote(parent, args, context, info) {
+    const { pollId } = args
+    return context.db.mutation.createVote({ 
+      data: {
+        option: { connect: { id: pollId } }
+      } 
+    }, info)
   }
 
 module.exports = {
-  postPoll
+  postPoll,
+  vote
 }

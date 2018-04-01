099ea3d478a3e6f5ee30d3491e4f8040330dd7ec
Peter Daily <peter.daily@yahoo.co.uk>
2018-04-01 11:13:54 +0100

    add User data model and authentication

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,8 +1,47 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { APP_SECRET, getUserId } = require('../utils')
+
+async function signup(parent, args, context, info) {
+  const password = await bcrypt.hash(args.password, 10)
+  const user = await context.db.mutation.createUser({
+    data: { ...args, password }
+  })
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+  return {
+    token,
+    user
+  }
+}
+
+async function login(parent, args, context, info) {
+  const user = await context.db.query.user({ where: { email: args.email } })
+  if (!user) {
+    throw new Error(`Could not find user with email: ${args.email}`)
+  }
+
+  const valid = await bcrypt.compare(args.password, user.password)
+  if (!valid) {
+    throw new Error('Invalid password')
+  }
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+  return {
+    token,
+    user
+  }
+}
+
 function postPoll(parent, args, context, info) {
   const { name, options } = args
+  const userId = getUserId(context)
   return context.db.mutation.createPoll({ 
     data: { 
       name,
+      postedBy: { connect: { id: userId } },
       options: { create: options }
      } 
     }, info)
@@ -18,6 +57,8 @@ function postPoll(parent, args, context, info) {
   }
 
 module.exports = {
+  signup,
   postPoll,
-  vote
+  vote,
+  login
 }

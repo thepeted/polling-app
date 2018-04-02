90ac1faa72a602b29155da7a9ab03d4b2bf2397e
Peter Daily <peter.daily@yahoo.co.uk>
2018-04-02 19:51:19 +0100

    remove service worker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import registerServiceWorker from './registerServiceWorker'
 
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
@@ -45,4 +44,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 )
-registerServiceWorker()
